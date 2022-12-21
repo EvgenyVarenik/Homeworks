b9ee87d6b03c09c5364b69a1a0250e7fd41e53d6
EvgenyVarenik <gendr_007@mail.ru>
2022-12-21 23:43:43 +0300

    Add Task13

---
+++ Seminar2/Homework_2/Task13/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+number = Math.Abs(number);
+
+if (number/100 > 0)
+{
+    while (number/10 > 99)
+    {
+        number=number/10;
+    }
+    int res = number%10;
+    Console.WriteLine(res);
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
