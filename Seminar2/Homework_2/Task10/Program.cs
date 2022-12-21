7655001585122ede69754a77b2a5df7e2acf427b
EvgenyVarenik <gendr_007@mail.ru>
2022-12-21 23:43:27 +0300

    Add Task10

---
+++ Seminar2/Homework_2/Task10/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите первое целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if ((Math.Abs(number)>99 && Math.Abs(number)<1000))
+{
+    int res1 = number/10;
+    int res2 = res1%10;
+    Console.WriteLine($"Вторая цифра числа: {Math.Abs(res2)}");
+}
+
