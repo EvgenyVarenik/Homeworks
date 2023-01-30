f0d42c2bbd01491d6cf30f34316a3f5cb5ff1b2e
EvgenyVarenik <gendr_007@mail.ru>
2023-01-30 23:19:48 +0300

    Add Task64,66,68

---
+++ Seminar9/Homework/Task66/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+Console.Write($"Число M = ");
+int M = Convert.ToInt32(Console.ReadLine()); 
+Console.Write($"Число N = ");
+int N = Convert.ToInt32(Console.ReadLine()); 
+
+int SumNum (int a, int b)
+{
+    if (a == b+1) return 0;
+    return (a + SumNum(a+1,b));
+    
+}
+
+
+Console.WriteLine($"{SumNum(M,N)}");
---
+++ Seminar9/Homework/Task68/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+
+Console.Write($"Число M = ");
+int M = Convert.ToInt32(Console.ReadLine()); 
+Console.Write($"Число N = ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int Akkerman (int a, int b)
+{
+    if (a == 0 && b > 0) return b+1;
+    else if (a > 0 && b == 0) return Akkerman(a-1,1);
+    else if (a > 0 && b > 0) return Akkerman(a-1,(Akkerman(a, b-1)));
+    else return 0;
+}
+
+Console.WriteLine($"{Akkerman(M,N)}");
